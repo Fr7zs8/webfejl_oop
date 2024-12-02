670a8b32e540488d0aaa035f32621f6d32bb7527
Sipos Fruzsina <siposfruzsina07@gmail.com>
2024-12-02 16:01:44 +0100

    Personnak lehet nevet allitani

--- basic/script.js
+++ basic/script.js
@@ -45,21 +45,22 @@ player1.play();
 printTierLevel(player1);
 
 
-function Person(){
-    this.name = "Géza";
+function Person(name){
+    this.name = name;
 }
 Person.prototype.getname = function(){
     return this.name;
 }
 
-function Student(school){
-    Person.call(this);
+function Student(school, name){
+    Person.call(this, name);
     this.school = school;
+    this.name = name;
 }
 
 Object.setPrototypeOf(Student.prototype, Person.prototype);
 
-let student1 = new Student("Bolyai");
+let student1 = new Student("Bolyai", "Fruzsi");
 
 console.log(student1.getname());
 console.log(student1.school);
