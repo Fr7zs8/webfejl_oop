56d18ca8c3b821157216f01dd6d42ec2be27e12f
sipos.fruzsina <siposfruzsina07@gmail.com>
2024-12-06 09:46:10 +0100

    Add table manos.

---
+++ santa/classes.js
@@ -0,0 +1,31 @@
+class Factory{ // TODO 1, 2, 3, 4, 9, 10
+    constructor(){
+        this.manok = new Array(); // egyenlő []
+
+    }
+    addMano(egymano) {
+        this.manok.push(egymano);
+        createRow(egymano);
+    }
+ 
+}
+
+class Companion{// TODO 5
+    constructor(id, keresztnev, vezeteknev, reszleg){
+        this.id = id;
+        this.keresztnev = keresztnev;
+        this.vezeteknev = vezeteknev;
+        this.reszleg = reszleg;
+        this.products = new Array();
+    }
+    addproduct(product){
+        this.products.push(product);
+    }
+
+    getName(){
+        return this.vezeteknev + " " + this.keresztnev
+    }
+ 
+}
+let mano = new Companion(1, "Kristály", "Szép", "Sátor");
+console.log(mano.getName())
---
+++ santa/main.js
@@ -0,0 +1,76 @@
+/**
+ * @type {{
+ *   firstName: string;
+ *   lastName: string;
+ *   products: string[];
+ *   area: string;
+ * }[]}
+ */
+const companionList = [
+    {
+        firstName: 'Géza',
+        lastName: 'Kiss',
+        area: 'plastic',
+        products: ['kisauto', 'barbibaba']
+    },
+    {
+        firstName: 'Ferenc',
+        lastName: 'Tóth',
+        area: 'paper',
+        products: ['könyv']
+    },
+    {
+        firstName: 'Gábor',
+        lastName: 'Nagy',
+        area: 'plastic',
+        products: ['zokni']
+    },
+]
+const factory = new Factory();
+
+document.getElementById('companion').addEventListener('submit',function(e){
+    e.preventDefault();
+    const form =  e.currentTarget
+    addCompanion(form, factory);
+});
+
+document.getElementById('product').addEventListener('submit',function(e){
+    e.preventDefault();
+    const form = e.currentTarget;
+    addProductForm(form, factory)
+});
+
+/**
+ * table render
+ */
+function initTable(){// TODO 6
+    for(let i = 0; i<companionList.length; i++){ // végig iterál az arryan
+        const mano = companionList[i]; // a listábol az i edik elemét kiszedjük
+        const actualmano = new Companion(i, mano.firstName, mano.lastName, mano.area); // companion objektum létrehozása  és a konstructorra példányositunk
+        for(let product of mano.products){ // A mano objektum product tömbjén megy végig
+            actualmano.addproduct(product); // hozzáadja az elemet
+        }
+        /*
+        console.log(actualmano);
+        console.log(actualmano.manoNameJelen());
+        */
+        factory.addMano(actualmano);
+    }
+    
+    
+}
+
+
+initTable()
+
+/**
+ * 
+ * eventlistener callback for the button click of companion
+ * 
+ * @param {EventTarget} e 
+ */
+function checkEventListener(e){
+    const row = e.currentTarget.parentElement.parentElement;
+    const companionId = row.id;
+    // TODO 10
+}
