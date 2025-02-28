7ecde41d02452fc9f369b9ebdcd76fb8652f7c91
sipos.fruzsina <siposfruzsina07@gmail.com>
2025-02-28 15:38:58 +0100

    Kesz a program

--- select/app/select/area.js
+++ select/app/select/area.js
@@ -1,21 +1,31 @@
 class Area {
      #div;
+    #manager
+
      get div(){
          return this.#div;
      }
+     get manager(){
+        return this.#manager;
+     }
 
      constructor(cssclass,manager){
+        this.#manager = manager;
         const container = this.#getContainer()
         this.#div = document.createElement('div');
         this.#div.className = cssclass;
         container.appendChild(this.#div);
-        manager.setFinishCallback(result => {
-            container.innerHTML = '';
+        this.#manager.setFinishCallback(this.#finishCallback(container));
+     }
+
+     #finishCallback(containerDiv){
+        return result => {
+            containerDiv.innerHTML = '';
             const div = document.createElement('div');
             div.className = 'result';
             div.textContent = result;
-            container.appendChild(div);
-        })
+            containerDiv.appendChild(div);
+        }
      }
  
      #getContainer(){
@@ -33,22 +43,28 @@ class DeckArea extends Area{
 
     constructor(cssClass, manager){
         super(cssClass, manager);
-        manager.setNextCardCallback(answer => {
+        manager.setNextCardCallback(this.#nextCardCallback());
+    }
+
+    #nextCardCallback(){
+        return answer => {
             this.div.innerHTML = '';
             const skip = document.createElement('button');
-            skip.addEventListener('click', () => {
-                manager.nextCard();
-            })
+            skip.addEventListener('click', this.#clickOnCardOrSkip())
             skip.textContent = 'skip';
             this.div.appendChild(skip);
             const cardElement = document.createElement('div');
             cardElement.textContent = answer;
             cardElement.className = 'largecard';
-            cardElement.addEventListener('click', () => {
-                manager.nextCard(answer)
-            })
+            cardElement.addEventListener('click', this.#clickOnCardOrSkip(answer))
             this.div.appendChild(cardElement);
-        })
+        }
+    }
+
+    #clickOnCardOrSkip(answer){
+        return () => {
+            this.manager.nextCard(answer)
+        }
     }
 
 }
@@ -58,11 +74,15 @@ class SolutionArea extends Area{
 
     constructor(cssClass, manager){
         super(cssClass, manager);
-        manager.setAppendCardToSolutionCallback(answer => {
+        this.manager.setAppendCardToSolutionCallback(this.#appendToCardSolution())
+    }
+
+    #appendToCardSolution(){
+        return answer => {
             const card = document.createElement('div');
             card.className = 'card';
             card.textContent = answer;
             this.div.appendChild(card);
-        })
+        }
     }
 }
